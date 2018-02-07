202eeb5ce796620172b95dccd33138ee9435242d
Raissa Policarpio <raissa.policarpio@gmail.com>
2018-02-06 16:10:25 -0800

    add header

--- persona5/src/App.js
+++ persona5/src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import protagonist from './img/protagonist.png';
+import p5logo from './img/logo.png';
+
 import { Nav, NavItem } from 'react-bootstrap';
 import { NegotiationTableWrapper } from './components/negotiation_table';
 
 class BlankPage extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
   }
 }
 
@@ -54,14 +67,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="header">
+          <img src={protagonist} className="pull-left" />
+          <img src={p5logo} className="logo" />
+          <div className="clearfix"></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
         <NavBar activePage={this.state.page} onSelect={this.setPage} />
         {PAGES[this.state.page].page}
       </div>
